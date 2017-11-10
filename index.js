1ef0afb9a98efc9d7c68b6f3f93c8bfa86520583
Angela <wang.angela90@gmail.com>
2017-11-10 00:39:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function destructivelyRemoveFirstKitten(name){
 }
 
 function appendKitten(name) {
+  console.log(kittens)
   var newArray = kittens;
   newArray.push(name);
   console.log(kittens)
