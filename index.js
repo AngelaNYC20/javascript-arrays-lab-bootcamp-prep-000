7dcb311446253d8819206f517fa3b28bd5c6e2d6
Angela <wang.angela90@gmail.com>
2017-11-10 00:51:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,3 +26,8 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [name, ...kittens];
 }
+
+function removeLastKitten(name) {
+  var newArray = kittens.slice(2);
+  return newArray;
+}
